3430b548b6f78d7c608b86375f1a1a94e8b35b2c
Esteban82 <federico.esteban@gmail.com>
2022-11-25 17:38:35 -0300

    viernes

--- 6_SEGY_ExtraerCoord_secarc2degree/SEGY_ExtraerCoord_secarc2degree.py
+++ 6_SEGY_ExtraerCoord_secarc2degree/SEGY_ExtraerCoord_secarc2degree.py
@@ -3,14 +3,15 @@
 Creado por Sebastian Principi y Federico Esteban
 28-09-2022 
 Extrae las coordendas de los shotpoints en segundos de arco 
-y los guarda en un archivo csv como grados decimales.
+y los guarda en un archivo csv (uno por cada Segy) como grados decimales
+y con los datos del FFID (byte 9 del trace header).
 """
 
 # Variables a modificar
 # --------------------------------------------------------------
 # Directorio a analizar
 #path=r"C:\Users\usuario\Desktop\Nueva carpeta"
-path=r"E:\m783b\0_DatosCrudos\1_SGY"
+path=r"/home/federico/Github/Geoflama/SEGY/0_DatosPrueba"
 
 # Inicio Script
 # --------------------------------------------------------------
@@ -35,14 +36,17 @@ for file in os.listdir(path):
             # C. Leer los header de X e Y y los paso de segundos de arco a grados
             sourceX = f.attributes(segyio.TraceField.SourceX)[:]/36000000
             sourceY = f.attributes(segyio.TraceField.SourceY)[:]/36000000
+
+            # Leer datos de FFID (byte 9)
+            ffid = f.attributes(segyio.TraceField.FieldRecord)[:]
             
             # D. Imprimir en la terminal
             print(sourceX)
             print(sourceY)
+            print (ffid)
 
         # E. Convierto las listas de valores a un dataframe
-        df = pd.DataFrame(list(zip(sourceX, sourceY)),columns =['X', 'Y'])
-
-        # F. Guardo los datos como archivos de texto
-        df.to_csv(input_+".csv", index=False,sep=",")  
-        df.to_csv(input_+".txt", index=False,sep=",") 
+        df = pd.DataFrame(list(zip(sourceX, sourceY, ffid)),columns =['#Long', 'Lat', "FFID"])
+        
+        # F. Guardo los datos como archivos de texto (uno por cada segy) en un CCSV
+        df.to_csv(input_+".csv", index=False,sep=",")
