1d96eee6f8bca4b427c40a54d3761b23fe14e51b
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2022-11-26 08:54:20 -0300

    nuevo

--- 6_SEGY_ExtraerCoord_secarc2degree/SEGY_ExtraerCoord.py
+++ 6_SEGY_ExtraerCoord_secarc2degree/SEGY_ExtraerCoord.py
@@ -2,9 +2,11 @@
 """
 Creado por Sebastian Principi y Federico Esteban
 28-09-2022 
-Extrae las coordendas de los shotpoints en segundos de arco 
-y los guarda en un archivo csv (uno por cada Segy) como grados decimales
-y con los datos del FFID (byte 9 del trace header).
+Extrae las coordendas de los shotpoints y crea un tabla con los datos de navegacion
+Particularidades:
+1. Lee los bytes del trace header que dice tipo de coordenadas (bytes 89-90) y escalar (71-72).
+2. Genera tabla con datos del Long, Lat, Nombre SEGY, FFID.
+3. Genera una unica tabla
 """
 
 # Variables a modificar
@@ -32,7 +34,17 @@ for file in os.listdir(path):
         
         # B. Abrir el archivo como read write
         with segyio.open(input_, "r+", ignore_geometry=True) as f:
-            
+
+            # D. Leer los bytes con informacion de los trace header
+            # Scalar to All Coordinates (byte 71-72)
+            #SAC = f.attributes(segyio.TraceField.SAC)[:]
+            SAC = f.attributes(71)[:]
+            # if 0, then s x1, 1
+
+            # Coordinates Units (byte 89-90)
+            CU = f.attributes(89)[:]
+            # 1 : lenm/ft, 2: secarc, 3: deg, 4: DDMMSS
+
             # C. Leer los header de X e Y y los paso de segundos de arco a grados
             sourceX = f.attributes(segyio.TraceField.SourceX)[:]/36000000
             sourceY = f.attributes(segyio.TraceField.SourceY)[:]/36000000
@@ -48,5 +60,6 @@ for file in os.listdir(path):
         # E. Convierto las listas de valores a un dataframe
         df = pd.DataFrame(list(zip(sourceX, sourceY, ffid)),columns =['#Long', 'Lat', "FFID"])
         
+
         # F. Guardo los datos como archivos de texto (uno por cada segy) en un CCSV
         df.to_csv(input_+".csv", index=False,sep=",")
