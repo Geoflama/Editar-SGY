6c29c46d3f8f915746ec78cd43301cacba8f2474
Sebastian Principi <45643116+sebaprincipi@users.noreply.github.com>
2022-12-01 10:16:00 -0300

    Update SEGY_Editar_BytesCorrelativo.py

--- 7_SEGY_ConvertirCoord_geo2UTM20/SEGY_Editar_BytesCorrelativo.py
+++ 7_SEGY_ConvertirCoord_geo2UTM20/SEGY_Editar_BytesCorrelativo.py
@@ -5,29 +5,19 @@ Creado por Sebastian Principi y Federico Esteban
 Editar los bytes 71 y 89 de los Segy en UTM20S
 """
 
-# Variables a modificar
-# --------------------------------------------------------------
-# Directorio a analizar
-#path=r"E:\1.Meteor2009\M78a\M78-3a PS03\segy_unidos\Nueva carpeta"
-#path=r"D:\4_Segy_UTM"
-path=r"D:\M783b\2_UnirSegy_SGY_UTM"
-#path=r"D:\Prueba"
-
-# Inicio Script
-# --------------------------------------------------------------
-# 0. Importar librerias
-import os
+# Importo las librerias
 import segyio
+import os
 
-# 1. Cambiar a la carpeta path
-os.chdir(path)
+# Directorio a analizar
+path=r"D:\M783b\2_UnirSegy_SGY_UTM"
 
-# 2. Aplicar ciclo
 for file in os.listdir(path):
 
     # A. Abrir todos los *.sgy de la carpeta
     if file.endswith(".sgy"):
         input_=str(file)
+
         
         # C. Abrir el archivo como read write
         with segyio.open(input_, "r+", ignore_geometry=True) as f:
@@ -36,6 +26,8 @@ for file in os.listdir(path):
             for i in range(0,len(f.header)):
                 
                 # E. Numerar (desde 1) los bytes 09 (FFID), 17 (SP) y 21 (CDP).
-                #f.header[i][segyio.TraceField.CoordinateUnits]=1
+                f.header[i][segyio.TraceField.FieldRecord]=i+1
+                f.header[i][segyio.TraceField.EnergySourcePoint]=i+1
+                f.header[i][segyio.TraceField.CDP]=i+1
                 
                 
