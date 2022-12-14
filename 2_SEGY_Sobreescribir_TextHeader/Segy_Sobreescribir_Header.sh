693f329f4ae8e970618febbe370b423f51d8094d
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2022-12-14 09:53:36 -0300

    Sobre_escribir_Batch

--- 2_SEGY_Sobreescribir_TextHeader/Segy_Sobreescribir_Header.sh
+++ 2_SEGY_Sobreescribir_TextHeader/Segy_Sobreescribir_Header.sh
@@ -10,4 +10,4 @@ out=test.sgy
 # NOTA: Escribir SOLO 3200 bytes (40 filas de 80 caracteres).
 
 # Sobreescribir (notrunc) solo el comienzo (los 3200 bytes iniciales) del archivo de salida.
-dd conv=notrunc if=NewTextHeader.txt of=$out bs=3200 count=1 conv=notrunc
+dd conv=notrunc if=NewTextHeader.txt of=$out bs=3200 count=1 #conv=notrunc
