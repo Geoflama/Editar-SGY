519c8102584404baadd29c1785c0c424f6e9c663
Esteban82 <federico.esteban@gmail.com>
2022-12-14 10:36:14 -0300

    nulos

--- 2_SEGY_Sobreescribir_TextHeader/Segy_Sobreescribir_Header.sh
+++ 2_SEGY_Sobreescribir_TextHeader/Segy_Sobreescribir_Header.sh
@@ -9,5 +9,9 @@ out=test.sgy
 # Editar archivo NewTextHeader.txt.
 # NOTA: Escribir SOLO 3200 bytes (40 filas de 80 caracteres).
 
+
+# Escribir con 0 (borrar) el comienzo (los 3200 bytes iniciales) del archivo de salida.
+dd conv=notrunc if=/dev/zero of=$out bs=3200 count=1
+
 # Sobreescribir (notrunc) solo el comienzo (los 3200 bytes iniciales) del archivo de salida.
 dd conv=notrunc if=NewTextHeader.txt of=$out bs=3200 count=1 #conv=notrunc
